386f9607b8756efb6059855d9c9a7a3281cfc704
Jhoneiro <jhoneiro12@hotmail.com>
2025-05-20 09:21:08 -0500

    css integrado

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.data.database import init_db
 from src.gui.main_window import MainWindow
 from PyQt5.QtWidgets import QApplication
-import sys
+import sys, os
 
 if __name__ == "__main__":
     # 1) Inicializa y seedea la BD
@@ -9,6 +9,10 @@ if __name__ == "__main__":
 
     # 2) Arranca la aplicación
     app = QApplication(sys.argv)
+    qss_path = os.path.join(os.path.dirname(__file__), "style.qss")
+    with open(qss_path, "r", encoding="utf-8") as f:
+        app.setStyleSheet(f.read())
+    
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
